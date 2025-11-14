7fc0f641286881d7465a9bbaed3f2e30aeda46e2
andrew.harrison <andrew.harrison@Defra.onmicrosoft.com>
2025-11-14 15:29:36 +0000

    INSC-39 Updated the query to reflect the postgres equivalent of the SQL Server origin

--- src/main/java/uk/gov/defra/cdp/trade/demo/domain/repository/CommodityAttributeRepository.java
+++ src/main/java/uk/gov/defra/cdp/trade/demo/domain/repository/CommodityAttributeRepository.java
@@ -1,6 +1,7 @@
 package uk.gov.defra.cdp.trade.demo.domain.repository;
 
 import java.util.List;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import uk.gov.defra.cdp.trade.demo.domain.CommodityAttribute;
@@ -8,7 +9,12 @@ import uk.gov.defra.cdp.trade.demo.domain.CommodityAttribute;
 @Repository
 public interface CommodityAttributeRepository extends
     CrudRepository<CommodityAttribute, String> {
-
+  @Query(nativeQuery = true, value = "select distinct result.code, result.traces_commodity_code, "
+          + "result.propagation "
+          + "from commodity_attributes ca inner join lateral"
+          + "(select * from commodity_attributes where "
+          + "commodity_attributes.traces_commodity_code = ca.traces_commodity_code limit 1) "
+          + "result on ca.traces_commodity_code IN :commodityCodes")
   List<CommodityAttribute> findAllByCommodityCodeIn(
       List<String> commodityCodes);
 }
