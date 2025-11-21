59d1081f579bb6374f035696617388b9f1e89c51
andrew.harrison <andrew.harrison@Defra.onmicrosoft.com>
2025-11-21 10:36:03 +0000

    INSC-39 updating the config to allow toggling of metrics.  Remove unecessary code

--- src/main/java/uk/gov/defra/cdp/trade/demo/configuration/MetricsConfig.java
+++ src/main/java/uk/gov/defra/cdp/trade/demo/configuration/MetricsConfig.java
@@ -22,38 +22,6 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class MetricsConfig {
 
-  private static final String[] IGNORED_METRICS = {"jvm.compilation.time",
-      "tasks.scheduled.execution.active", "jvm.memory.used.bytes", "jvm.memory.committed.bytes", "jvm.memory.max.bytes",
-      "jvm.gc.max.data.size", "jvm.gc.live.data.size", "jvm.gc.memory.promoted", "jvm.gc.memory.allocated",
-      "jvm.threads.live", "jvm.threads.daemon", "jvm.threads.peak", "jvm.threads.states", "jvm.threads.classes",
-      "jvm.threads.started", "jvm.threads.deadlocked", "jvm.threads.deadlocked.monitor", "jvm.threads.state.new",
-      "jvm.threads.state.new.count", "jvm.threads.state.runnable", "jvm.threads.state.runnable.count",
-      "jvm.threads.state.blocked", "jvm.threads.state.blocked.count", "jvm.threads.state.waiting"};
-
-  private static final String[] ACCEPTED_METRICS = {"jvm.memory.used", "jvm.memory.committed",
-      "jvm.memory.max"};
-
-  private static final String[] PROCESS_METRICS = {"process.cpu.usage"};
-
-  private static final String[] HTTP_METRICS = {"http.server.requests"};
-
-  private static final String[] HIKARI_METRICS = {"hikaricp.connections.max", "hikaricp.connections.active",
-      "hikaricp.connections", "hikaricp.connections.acquire", "hikaricp.connections.usage"};
-
-  private static final String[] LOGBACK_METRICS = {"logback.events", "logback.events.total"};
-
-  private static final String[] TOMCAT_METRICS = {"tomcat.sessions.active.current", "tomcat.sessions.active.max",
-      "tomcat.sessions.created", "tomcat.sessions.rejected", "tomcat.sessions.expired", "tomcat.sessions.accessed"};
-
-  private static final String[] EXECUTOR_METRICS = {"executor.completed", "executor.queued", "executor.active",
-      "executor.pool.size", "executor.pool.active", "executor.pool.largest"};
-
-  private static final String[] APPLICATION_METRICS = {"application.started.timestamp", "application.uptime",
-      "application.status", "application.name", "application.version"};
-  private static final String[] DISK_METRICS = {"disk.free", "disk.total", "disk.usable"};
-
-  private static final String[] JDBC_METRICS = {"jdbc.connections.active", "jdbc.connections.max", "jdbc.connections.min", "jdbc.connections.idle"};
-
   @Bean
   public TimedAspect timedAspect(MeterRegistry registry) {
     log.debug("Creating TimedAspect for {}", registry.getClass().getSimpleName());
